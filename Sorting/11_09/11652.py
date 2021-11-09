6e2c6b7a558cf74e94452ed2c93f90f32e629379
ili0820 <2hjc12@gmail.com>
2021-11-09 11:59:13 +0900

    11_09

--- Sorting/11_09/11652.py
+++ Sorting/11_09/11652.py
@@ -3,7 +3,9 @@ from collections import Counter
 input=sys.stdin.readline
 n=int(input())
 numbs=[int(input()) for _ in range(n)]
-numbs=Counter(numbs).most_common()
-numbs=sorted(numbs,key=lambda x : (-x[1],x[0]))
+numbs=sorted(numbs)
+numbs=Counter(numbs).most_common(1)
+# print(numbs)
+# numbs=sorted(numbs,key=lambda x : (-x[1],x[0]))
 
 print(numbs[0][0])
